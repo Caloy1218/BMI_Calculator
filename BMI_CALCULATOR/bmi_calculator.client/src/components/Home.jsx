e4d4335407d86f910a65db66302efa471ae4633f
Caloy1218 <jcsaquing1218@gmail.com>
2023-12-02 01:42:15 +0800

    Added UI

--- BMI_CALCULATOR/bmi_calculator.client/src/components/Home.jsx
+++ BMI_CALCULATOR/bmi_calculator.client/src/components/Home.jsx
@@ -1,7 +1,68 @@
+import "./Home.css"
+import React, { useState } from 'react';
 function Home() {
+    const [gender, setGender] = useState('');
+    const [isMetric, setIsMetric] = useState(true);
+
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+    };
+
+    
+
+    const handleUnitToggle = () => {
+        setIsMetric(!isMetric);
+    };
     return (
-        <div>
-            <h1>HELLO WORLD</h1>
+        <div className="home--container">
+            <hi className="title">BMI Calculator</hi>
+            <div className="home--genderMetric">
+                <div className ="genderContainer">
+                    <label>
+                        <input type="radio"
+                                name="gender"
+                                value="female"
+                                className="female"
+                                checked={gender === 'female'}
+                            onChange={handleGenderChange} />
+                                
+                         Female
+                    </label>
+                    <label>
+                        <input type="radio"
+                          name="gender"
+                          value="male"
+                          className="male"
+                          checked={gender === 'male'}
+                          onChange={handleGenderChange}
+                        />
+                        Male
+                  </label>
+                </div>
+                <div className="metricContainer">
+                    <label>
+                        <button onClick={handleUnitToggle} className="toggle--units">{isMetric ? 'Metric' : 'Imperial'}</button>
+                    </label>
+                </div>
+            </div>
+            <div className="home--AgeHW">
+                <div className="comp-container">
+                    <label>Age: </label>
+                    <input type="number" />
+                </div>
+                <div className="comp-container">
+                    <label>Weight ({isMetric ? 'kg' : 'lb'}): </label>
+                    <input type="number" />
+                </div>
+                <div className="comp-container">
+                    <label>Height ({isMetric ? 'cm' : `Ft' in`})</label>
+                    <input type="number" />
+                </div>
+                
+                
+               
+                <button className="calculate">Calculate</button>
+            </div>
         </div>
     )
 }
